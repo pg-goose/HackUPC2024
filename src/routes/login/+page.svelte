31ef07746e2749162fd4f1ec70cadd94d9885bf9
AndreaFrutos <andrea.frutos@estudiantat.upc.edu>
2024-05-04 13:44:16 +0200

    paginas :)

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,12 +2,64 @@
   let email = '';
   let password = '';
 
-  function handleSubmit() {
-    // Aquí puedes agregar la lógica para manejar el envío del formulario
-    console.log('Correo electrónico:', email);
-    console.log('Contraseña:', password);
+  async function handleSubmit() {
+    // Realiza la solicitud POST para enviar los datos del formulario a la base de datos
+    
+    
+      window.location.href = '/home';
   }
+    
 </script>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center; /* Centrar horizontalmente */
+    justify-content: center; /* Centrar verticalmente */
+    height: 100vh; /* Altura completa de la ventana */
+  }
+  
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center; /* Centrar horizontalmente */
+    width: 300px;
+  }
+
+  input {
+    margin-bottom: 10px;
+    padding: 8px;
+    width: 100%;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
+
+  button {
+    padding: 10px 20px;
+    background-color: #0693e3;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+  }
+
+  button:hover {
+    background-color: #007acc;
+  }
+
+  .register-link {
+    margin-top: 10px;
+    font-size: 14px;
+    color: #0693e3;
+    text-decoration: none;
+  }
+
+  .register-link:hover {
+    text-decoration: underline;
+  }
+</style>
 
 <div class="container">
   <h2>Iniciar sesión</h2>
@@ -16,4 +68,5 @@
     <input type="password" placeholder="Contraseña" bind:value={password}>
     <button type="submit">Iniciar sesión</button>
   </form>
+  <a href="/register">¿No tienes cuenta? Regístrate aquí</a>
 </div>
